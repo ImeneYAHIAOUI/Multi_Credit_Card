9cf97f7c93c33dc57aedc1524e489fd575cbddb8
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2023-04-04 15:59:52 +0200

    change artifacte names

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,5 @@
 
 echo "Compiling the TCF Spring BACKEND within a multi-stage docker build"
 
-docker build --build-arg JAR_FILE=backend-0.0.1-SNAPSHOT.jar -t equipef/tcf-spring-backend .
+docker build --build-arg JAR_FILE=multicard-backend-0.0.1-SNAPSHOT.jar -t equipef/tcf-spring-backend .
 
