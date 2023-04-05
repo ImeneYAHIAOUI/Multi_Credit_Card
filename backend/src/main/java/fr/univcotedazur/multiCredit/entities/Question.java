dee2dffccf215f28b5e48f89a493cbb36c1d83bc
AureliaChabanier <aurelia.chabanier@etu.unice.fr>
2023-04-05 16:17:46 +0200

    fix question #54

--- backend/src/main/java/fr/univcotedazur/multiCredit/entities/Question.java
+++ backend/src/main/java/fr/univcotedazur/multiCredit/entities/Question.java
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream:backend/src/main/java/fr/univcotedazur/simpletcfs/entities/Question.java
-package fr.univcotedazur.simpletcfs.entities;
+package fr.univcotedazur.multiCredit.entities;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -16,28 +15,14 @@ public class Question {
     @Id
     @GeneratedValue
     private Long id;
-=======
-package fr.univcotedazur.multiCredit.entities;
-
-import java.util.List;
-
-public class Question {
-    private String title;
-    private List<String> possibleAnswers;
->>>>>>> Stashed changes:backend/src/main/java/fr/univcotedazur/multiCredit/entities/Question.java
 
     public Question(String title, List<String> possibleAnswers) {
         this.title = title;
         this.possibleAnswers = possibleAnswers;
     }
-
-<<<<<<< Updated upstream:backend/src/main/java/fr/univcotedazur/simpletcfs/entities/Question.java
     public Question() {
 
     }
-
-=======
->>>>>>> Stashed changes:backend/src/main/java/fr/univcotedazur/multiCredit/entities/Question.java
     public String getTitle() {
         return title;
     }
@@ -53,7 +38,6 @@ public class Question {
     public void setPossibleAnswers(List<String> possibleAnswers) {
         this.possibleAnswers = possibleAnswers;
     }
-<<<<<<< Updated upstream:backend/src/main/java/fr/univcotedazur/simpletcfs/entities/Question.java
 
     public void setId(Long id) {
         this.id = id;
@@ -62,6 +46,4 @@ public class Question {
     public Long getId() {
         return id;
     }
-=======
->>>>>>> Stashed changes:backend/src/main/java/fr/univcotedazur/multiCredit/entities/Question.java
 }
